af899f0635d13cb0d6c789031c46d15e98d1ce9c
AlexandreRocchi <90569500+AlexandreRocchi@users.noreply.github.com>
2022-10-27 15:11:51 +0200

    2nd Commit

--- Jump/Form1.Designer.cs
+++ Jump/Form1.Designer.cs
@@ -28,21 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.platform1 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.platform1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             this.SuspendLayout();
+            // 
+            // platform1
+            // 
+            this.platform1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.platform1.Location = new System.Drawing.Point(24, 438);
+            this.platform1.Name = "platform1";
+            this.platform1.Size = new System.Drawing.Size(199, 24);
+            this.platform1.TabIndex = 0;
+            this.platform1.TabStop = false;
+            this.platform1.Tag = "platform";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.player.Location = new System.Drawing.Point(45, 380);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(31, 40);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            this.player.Tag = "player";
+            // 
+            // gametimer
+            // 
+            this.gametimer.Tag = "gametimer";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(867, 484);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.platform1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.platform1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox platform1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer gametimer;
     }
 }
 
--- Jump/Form1.cs
+++ Jump/Form1.cs
@@ -12,6 +12,19 @@ namespace Jump
 {
     public partial class Form1 : Form
     {
+        bool goLeft, goRight, jumping, IsGameOver;
+        bool enemy_direction;
+        int jumpSpeed;
+        int force;
+        int score = 0;
+        int playerSpeed = 10;
+
+        int horizontalSpeed = 5;
+        int verticalSpeed = 3;
+
+        int ennemyOneSpeed = 5;
+        int enemyTwoSpeed = 3;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +32,113 @@ namespace Jump
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            player.Top += jumpSpeed;
+
+            if (goLeft == true)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight == true)
+            {
+                player.Left += playerSpeed;
+            }
+
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "platform")
+                    {
+                        if (player.Bounds.IntersectsWith(x.Bounds) && !jumping)
+                        {
+                            Console.WriteLine("okokokooko");
+                            force = 8;
+                            player.Top = x.Top - player.Height;
+                            jumpSpeed = 0;
+                        }
+
+                        x.BringToFront();
+                    }
+                }
+            }
+         }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up && !jumping)
+            {
+                jumping = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+    
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+            if (e.KeyCode == Keys.Enter && IsGameOver == true)
+            {
+                ResetGame();
+            }
+        }
+
+        private void ResetGame()
+        {
+            jumping = false;
+            goLeft = false;
+            goRight = false;
+            IsGameOver = false;
+            score = 0;
+
+            //txtscore.Text = "Score : " + score;
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+            }
+
+            player.Left = 24;
+            player.Top = 687;
+
+            //enemyOne.Left = 475; 
+            //enemyTwo.Left = 794;
+
+            //horizontal.Left = 230;
+            //vertical.Top = 287;
+
+            gametimer.Start();
+
 
         }
     }
